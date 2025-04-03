bdb1a238c8b795f463e3f4db2ff29ebcc8fc9ab0
paulo <paulorafaelbrandao@gmail.com>
2025-04-03 15:57:27 -0300

    Desafio concluido

---
+++ desafio1.py
@@ -0,0 +1,16 @@
+h1 = int(input("Informe a primeira hora: "))
+m1 = int(input("Informe o primeiro minuto: "))
+h2 = int(input("Informe a segunda hora: "))
+m2 = int(input("Informe o segundo minuto: "))
+
+somahoras = h1 + h2
+somaminutos = m1 + m2
+
+if somaminutos >= 60:
+    somahoras = somahoras + 1
+    somaminutos = somaminutos - 60
+if somahoras > 12:
+    somahoras = somahoras - 12
+if somahoras > 24:
+    somahoras = somahoras - 24
+print (f"{somahoras} : {somaminutos}")
